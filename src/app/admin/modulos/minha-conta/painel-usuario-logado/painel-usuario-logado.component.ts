9ea86e24408ad98b73a69af4a5f16ca37df5a15c
leandrocodegit <lpoliveira.ti@gmail.com>
2026-06-19 16:25:29 -0300

    first commit

--- src/app/admin/modulos/minha-conta/painel-usuario-logado/painel-usuario-logado.component.ts
+++ src/app/admin/modulos/minha-conta/painel-usuario-logado/painel-usuario-logado.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { KeycloakUserProfileComponent } from '../keycloak/keycloak-user-profile/keycloak-user-profile.component';
 import { KeycloakSessionsComponent } from '../keycloak/keycloak-sessions/keycloak-sessions.component';
 import { KeycloakAccountSettingsComponent } from '../keycloak/keycloak-account-settings/keycloak-account-settings.component';
+import { UsuarioService } from '@/shared/services/usuario.service';
+import { AuthService } from '@/core/auth/services/auth.service';
 
 @Component({
   selector: 'app-painel-usuario-logado',
@@ -15,6 +17,16 @@ import { KeycloakAccountSettingsComponent } from '../keycloak/keycloak-account-s
   templateUrl: './painel-usuario-logado.component.html',
   styleUrl: './painel-usuario-logado.component.scss'
 })
-export class PainelUsuarioLogadoComponent {
+export class PainelUsuarioLogadoComponent implements OnInit {
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usuarioService: UsuarioService){}
+
+  ngOnInit(): void {
+
+    this.usuarioService.listaSessoesUsuarios(this.authService.extrairIdUsuario()).subscribe();
+
+  }
 
 }
