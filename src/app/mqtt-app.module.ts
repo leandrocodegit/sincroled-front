b4f61f283e28ac3a77c241ea8feaacabbfb04bb9
Leandro Oliveira <41707801+leandrocodegit@users.noreply.github.com>
2026-06-23 13:02:29 -0300

    Update mqtt-app.module.ts

--- src/app/mqtt-app.module.ts
+++ src/app/mqtt-app.module.ts
@@ -6,10 +6,8 @@ export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
   port: environment.portaWebSocket,
   protocol: environment.protocoloWebSocket,
   path: '/ws',
-  //username: 'cliente',
- // password: 'dbbb02ab86addf27'
- username: 'device',
- password: '74e892085fed204e'
+  username: 'cliente',
+  password: 'dbbb02ab86addf27'
 }
 //74e892085fed204e
 @NgModule({
